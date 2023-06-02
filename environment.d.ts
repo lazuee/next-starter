77ad1f54bbb5addd9ac4109ae3030e29fac9780c
John Marlo Lapiz <lazuee.dev@gmail.com>
2023-06-02 22:07:09 +0800

    Update

--- environment.d.ts
+++ environment.d.ts
@@ -1,9 +1,8 @@
 declare namespace NodeJS {
 	export interface ProcessEnv {
-		readonly NEXT_PUBLIC_APP_NAME: string;
-		readonly NEXT_PUBLIC_APP_URL: `https://${string}`;
+		readonly NEXT_PUBLIC_SITE_URL: `https://${string}`;
 
-		readonly NEXT_PUBLIC_GOOGLE_ANALYTICS: string;
 		readonly NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION: string;
+		readonly NEXT_PUBLIC_YANDEX_SITE_VERIFICATION: string;
 	}
 }
